80cc6a15bd65be03ad3871de784690ffd4c758c3
DavidCuturic5 <david.cuturic5@gmail.com>
2024-10-26 22:10:25 +0200

    Add files via upload

---
+++ klasaStringVjezba05/klasaStringVjezba05/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringVjezba05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite jedan cijeli i jedan decimalni broj odvojeni razmakom:");
+
+            string unos = Console.ReadLine();
+
+            string[] brojevi=unos.Split(' ');
+            int cijeli=int.Parse(brojevi[0]);
+            double decimalni=double.Parse(brojevi[1]);
+
+            int x = cijeli * 2;
+            double y = decimalni * 2;
+
+            Console.WriteLine($"Dvostruke vrijednosti unešenih brojeva su: {x} i {y}" );
+            Console.ReadKey(); 
+        }
+    }
+}
